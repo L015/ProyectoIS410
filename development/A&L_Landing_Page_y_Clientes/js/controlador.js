a7a97ba5d3479440a4323ad1d7c4c8b0353d01df
Darnai <107970801+Darnai@users.noreply.github.com>
2022-07-17 15:48:02 -0600

    Add files via upload

---
+++ development/A&L_Landing_Page_y_Clientes/js/controlador.js
@@ -0,0 +1,501 @@
+var ofertas = {
+    imagenOferta: "img/oferta.png",
+    imgCarrusel: "img/fotoCarrusel.webp",
+}
+
+var categorias = [
+    {
+        nombreCategoria: 'Juguetes',
+        productos: [
+            {
+                nombreProducto: 'Juguete 1',
+                imagenProduto: 'img/juguetes/juguete1.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Juguete 2 ',
+                imagenProduto: 'img/juguetes/juguete2.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Juguete 3',
+                imagenProduto: 'img/juguetes/juguete3.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Juguete 4',
+                imagenProduto: 'img/juguetes/juguete4.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Juguete 5',
+                imagenProduto: 'img/juguetes/juguete5.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Juguete 6',
+                imagenProduto: 'img/juguetes/juguete6.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Juguete 7',
+                imagenProduto: 'img/juguetes/juguete7.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            
+        ]
+    },
+    {
+        nombreCategoria: 'Muebles',
+        productos: [
+            {
+                nombreProducto: 'Mueble 1',
+                imagenProduto: 'img/muebles/mueble1.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+            {
+                nombreProducto: 'Mueble 2 ',
+                imagenProduto: 'img/muebles/mueble2.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+            {
+                nombreProducto: 'Mueble 3',
+                imagenProduto: 'img/muebles/mueble3.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+            {
+                nombreProducto: 'Mueble 4',
+                imagenProduto: 'img/muebles/mueble4.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+            {
+                nombreProducto: 'Mueble 5',
+                imagenProduto: 'img/muebles/mueble5.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+            {
+                nombreProducto: 'Mueble 6',
+                imagenProduto: 'img/muebles/mueble6.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+            {
+                nombreProducto: 'Mueble 7',
+                imagenProduto: 'img/muebles/mueble7.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+
+            },
+        ]
+    },
+    {
+        nombreCategoria: 'Tecnologia',
+        productos: [
+             {
+                nombreProducto: 'Electronico 1',
+                imagenProduto: 'img/electronicos/electronico1.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Electronico 2 ',
+                imagenProduto: 'img/electronicos/electronico2.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Electronico 3',
+                imagenProduto: 'img/electronicos/electronico3.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Electronico 4',
+                imagenProduto: 'img/electronicos/electronico4.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Electronico 5',
+                imagenProduto: 'img/electronicos/electronico5.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Electronico 6',
+                imagenProduto: 'img/electronicos/electronico6.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Electronico 7',
+                imagenProduto: 'img/electronicos/electronico7.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+        ]
+    },
+    {
+        nombreCategoria: 'Zapatos',
+        productos: [
+             {
+                nombreProducto: 'Zapatos 1',
+                imagenProduto: 'img/zapatos/zapato1.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Zapatos 2 ',
+                imagenProduto: 'img/zapatos/zapato2.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Zapatos 3',
+                imagenProduto: 'img/zapatos/zapato3.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Zapatos 4',
+                imagenProduto: 'img/zapatos/zapato4.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Zapatos 5',
+                imagenProduto: 'img/zapatos/zapato5.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Zapatos 6',
+                imagenProduto: 'img/zapatos/zapato6.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+            {
+                nombreProducto: 'Zapatos 7',
+                imagenProduto: 'img/zapatos/zapato7.png',
+                precio: '9999$',
+                descripcionProducto: 'Producto en tu carrito',
+            },
+        ]
+    },
+]
+
+var empresas = [
+    {
+        categoria: 'Electronicos',
+        empresasTipos: [
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/asus.png',
+                descripcionEmpresa: 'Esta es una empresa de productos tecnologicos',
+            },
+            {
+                nombreEmpresa: ' EVGA ',
+                imagenEmpresa: 'img/empresas/evga.png',
+                descripcionEmpresa: 'Esta es una empresa de productos tecnologicos',
+            },
+            {
+                nombreEmpresa: ' HP ',
+                imagenEmpresa: 'img/empresas/hp.png',
+                descripcionEmpresa: 'Esta es una empresa de productos tecnologicos',
+            },
+            {
+                nombreEmpresa: ' Logitech ',
+                imagenEmpresa: 'img/empresas/logitech.png',
+                descripcionEmpresa: 'Esta es una empresa de productos tecnologicos',
+            },
+            {
+                nombreEmpresa: ' Razer ',
+                imagenEmpresa: 'img/empresas/razer.png',
+                descripcionEmpresa: 'Esta es una empresa de productos tecnologicos',
+            },
+            {
+                nombreEmpresa: ' Samsung ',
+                imagenEmpresa: 'img/empresas/samsung.png',
+                descripcionEmpresa: 'Esta es una empresa de productos tecnologicos',
+            },
+        ]
+    },
+    {
+        categoria: 'Electronicos',
+        empresasTipos: [
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/asus.png',
+            },
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/evga.png',
+            },
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/hp.png',
+            },
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/logitech.png',
+            },
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/razer.png',
+            },
+            {
+                nombreEmpresa: ' Asus ',
+                imagenEmpresa: 'img/empresas/samsung.png',
+            },
+        ]
+    },
+]
+
+
+console.log(categorias);
+
+//Crea la landing
+
+for(let i = 1; i<4 ; i++){
+    document.getElementById('oferta '+i).innerHTML += `
+    <img src="${ofertas.imagenOferta}" class="col-12">
+    `
+}
+
+
+    document.getElementById('fotosCarrusel').innerHTML += `
+    <div class="carousel-item active" >
+    <img src="${ofertas.imgCarrusel}" class="d-block w-100" alt="...">          
+    </div>
+    <div class="carousel-item"  >
+    <img src="${ofertas.imgCarrusel}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" >
+    <img src="${ofertas.imgCarrusel}" class="d-block w-100" alt="...">         
+    </div>       
+    `;
+
+    document.getElementById('fotosCarrusel2').innerHTML += `
+    <div class="carousel-item active" >
+    <img src="${ofertas.imgCarrusel}" class="d-block w-100" alt="...">          
+    </div>
+    <div class="carousel-item"  >
+    <img src="${ofertas.imgCarrusel}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" >
+    <img src="${ofertas.imgCarrusel}" class="d-block w-100" alt="...">         
+    </div>       
+    `;
+
+
+    for(let i =0 ; i<categorias.length ; i++){
+        document.getElementById('productosLanding').innerHTML +=`
+        <div class="col float-end">
+            <div class="card" >
+            <h3 class="card-title mx-auto">${categorias[i].nombreCategoria}</h3>
+            <div class="row" id="categoriaL${i}">
+            </div>
+            </div>
+            </div>
+        `
+
+    }
+    
+    for(let i =0 ; i<categorias.length ; i++){
+        for(let j =0 ; j<3 ; j++){
+        document.getElementById('categoriaL'+i).innerHTML +=`
+              
+              <img class="col-6 mx-auto imgLanding" src="${categorias[i].productos[j].imagenProduto}" class="card-img-top" alt="...">
+             
+              
+           
+          `
+    }
+}
+
+
+//Funcion del boton Inicio
+function cargarLanding(){
+
+  
+
+    
+    
+    document.getElementById('landing').style.display = 'block'; 
+    document.getElementById('cat').style.display = 'none'; 
+    document.getElementById('emp').style.display = 'none';  
+    document.getElementById('car').style.display = 'none';   
+    document.getElementById('botonInicio').style = 'color: var(--bs-navbar-active-color);';
+    document.getElementById('botonCategorias').style = 'color: var(--bs-nav-link-hover-color);';
+    document.getElementById('botonEmpresas').style = 'color: var(--bs-navbar-hover-color);';
+   
+   
+}
+cargarLanding();
+
+//Funcion del boton Categorias
+
+function cargarCategorias(){
+    document.getElementById('categoriasTipos').innerHTML = '';
+    for(let i = 0 ; i<categorias.length; i++){
+        document.getElementById('categoriasTipos').innerHTML += `
+        <div class="titulosCategorias"><h4>${categorias[i].nombreCategoria}</h4></div>
+    
+        <div class="scrollLateral" id="categoria${i}">
+      
+     </div>
+        `;
+    }
+   
+    for(let i = 0 ; i<categorias.length ; i++){
+        for(let j = 0 ; j<categorias[i].productos.length; j++){
+            document.getElementById('categoria'+[i]).innerHTML +=
+            `
+            <div class="productoScroll">
+            <div id="productosCategorias">
+          <img src="${categorias[i].productos[j].imagenProduto}" class="card-img-top imagenCategoria" alt="...">
+            <h5 >${categorias[i].productos[j].nombreProducto}</h5>
+            </div>
+          
+        
+      </div>
+            `
+        }
+    }
+
+    
+    
+    document.getElementById('cat').style.display = 'block';  
+    document.getElementById('emp').style.display = 'none';   
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('car').style.display = 'none'; 
+    document.getElementById('botonCategorias').style = 'color: var(--bs-navbar-active-color);';
+    document.getElementById('botonInicio').style = 'color: var(--bs-nav-link-hover-color);';
+    document.getElementById('botonEmpresas').style = 'color: var(--bs-navbar-hover-color);';
+    
+}
+
+//Funcion del boton Empresas
+function cargarEmpresas(){
+  
+    
+
+    for(let i = 0 ;i<empresas.length;i++){
+        document.getElementById('empresasTipos').innerHTML = '';
+        for(let j = 0 ; j< empresas[0].empresasTipos.length;j++){
+
+            document.getElementById('empresasTipos').innerHTML +=`   
+            <div class="col">
+            <div class="card " style="max-width: 600px;">
+            <div class="row g-0" style="min-height:200px;">
+              <div class="col-md-4 imagenCardEmpresa">
+                <img src="${empresas[0].empresasTipos[j].imagenEmpresa}" class="img-fluid rounded-start imagenEmpresa  " alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${empresas[0].empresasTipos[j].nombreEmpresa}</h5>
+                  <p class="card-text">${empresas[0].empresasTipos[j].descripcionEmpresa}</p>
+                  <p class="card-text"><small class="text-muted"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+              `
+        }
+      
+    }
+    
+   
+    document.getElementById('cat').style.display = 'none';    
+    document.getElementById('landing').style.display = 'none';
+    document.getElementById('car').style.display = 'none'; 
+    document.getElementById('emp').style.display = 'block'; 
+    document.getElementById('botonEmpresas').style = 'color: var(--bs-navbar-active-color);';
+    document.getElementById('botonCategorias').style = 'color: var(--bs-navbar-hover-color);';
+    document.getElementById('botonInicio').style = 'color: var(--bs-nav-link-hover-color);';
+}
+
+function cargarCarrito(){
+  
+    
+
+    for(let i = 0 ;i<3;i++){
+        document.getElementById('carritoPag').innerHTML = '';
+        for(let j = 0 ; j< 4;j++){
+
+            document.getElementById('carritoPag').innerHTML +=`   
+            <div class="col">
+            <div class="card " style="max-width: 600px;">
+            <div class="row g-0" >
+              <div class="col-md-4 imagenCardCarrito">
+                <img src="${categorias[i].productos[j].imagenProduto}" class="img-fluid rounded-start imagenEmpresa  " alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${categorias[i].productos[j].nombreProducto}</h5>
+                  <p class="card-text">${categorias[i].productos[j].descripcionProducto}</p>
+                  <p class="card-text"><small class="text-muted">${categorias[i].productos[j].precio}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+              `
+        }
+      
+    }
+
+ 
+   
+    document.getElementById('cat').style.display = 'none';    
+    document.getElementById('landing').style.display = 'none';   
+    document.getElementById('emp').style.display = 'none'; 
+    document.getElementById('car').style.display = 'block'; 
+    document.getElementById('botonEmpresas').style = 'color: var(--bs-navbar-active-color);';
+    document.getElementById('botonCategorias').style = 'color: var(--bs-navbar-hover-color);';
+    document.getElementById('botonInicio').style = 'color: var(--bs-nav-link-hover-color);';
+}
+
+
+
+
+// Validaciones segun bootstrap
+(() => {
+    'use strict'
+  
+    
+    const forms = document.querySelectorAll('.needs-validation')
+  
+   
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
