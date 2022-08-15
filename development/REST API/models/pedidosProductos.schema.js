38f2c1abbd112a0780e3c9ced3a5aa5622737010
Darnai <107970801+Darnai@users.noreply.github.com>
2022-08-15 11:59:19 -0600

    Add files via upload

---
+++ development/REST API/models/pedidosProductos.schema.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var esquema = new mongoose.Schema({
+    numeroPedido: Number,
+    nombreProducto:   String,  
+    precio:   String,  
+    cantidad: Number
+});
+                                
+                                //nombre de la coleccion
+module.exports = mongoose.model('productos', esquema);
+                                            //el esquema que creamos
+                                            
