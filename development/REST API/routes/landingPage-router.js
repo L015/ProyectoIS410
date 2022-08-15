a7698e664ea5a584caafcfd7a32bc68b92e8271a
Luis Lainez <108846266+L015@users.noreply.github.com>
2022-08-15 00:54:22 -0600

    Delete development/REST API/routes directory

--- development/REST API/routes/landingPage-router.js
+++
@@ -1,6 +0,0 @@
-var express =require('express');
-var router = express.Router();
-
-router.use(express.static('A&L_Landing_Page_y_Clientes'))
-
-module.exports = router;
--- development/REST API/routes/motoristas-router.js
+++
@@ -1,6 +0,0 @@
-var express =require('express');
-var router = express.Router();
-
-router.use(express.static('A&L motoristas'))
-
-module.exports = router;
