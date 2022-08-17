eb6086f79ecd61fbd68e6cda6a21ce89de6081cd
Luis Lainez <108846266+L015@users.noreply.github.com>
2022-08-17 16:20:59 -0600

    Add files via upload

--- development/REST API/routes/administracion-router.js
+++ development/REST API/routes/administracion-router.js
@@ -1,11 +1,13 @@
 var express =require('express');
 var router = express.Router();
 var usuarioModel=require('../models/usuario.schema');
+var empresasModel=require('../models/empresas.schema');
+var productosModel=require('../models/productos.schema');
 var mongoose=require('mongoose');
 
 
 router.use(express.static('A&L administracion'));
-router.get('/usuarios',function(req,res){
+router.get('/usuarios/',function(req,res){
     usuarioModel.find({}).then((data)=>{
         res.send(data);
         res.end();
@@ -14,6 +16,51 @@ router.get('/usuarios',function(req,res){
         res.send(error);
         res.end();
     })
-})
+});
+router.put('/usuarios/:id/:validacion',function(req,res){
+    usuarioModel.updateOne({"_id":req.params.id},{$set:{"validado":req.params.validacion}}).then((data)=>{
+        res.send(data);
+        res.end();
+    })
+    .catch((error)=>{
+        res.send(error);
+        res.end();
+    })
+});
+
+router.get('/empresas/',function(req,res){
+    empresasModel.find({}).then((data)=>{
+        res.send(data);
+        res.end();
+    })
+    .catch((error)=>{
+        res.send(error);
+        res.end();
+    })
+});
+// router.put('/usuarios/:id/:validacion',function(req,res){
+//     usuarioModel.updateOne({"_id":req.params.id},{$set:{"validado":req.params.validacion}}).then((data)=>{
+//         res.send(data);
+//         res.end();
+//     })
+//     .catch((error)=>{
+//         res.send(error);
+//         res.end();
+//     })
+// });
+
+
+router.get('/productos/',function(req,res){
+    productosModel.find({}).then((data)=>{
+        res.send(data);
+        res.end();
+    })
+    .catch((error)=>{
+        res.send(error);
+        res.end();
+    })
+});
+
+
 
 module.exports = router;
--- development/REST API/routes/motoristas-router.js
+++ development/REST API/routes/motoristas-router.js
@@ -1,6 +1,21 @@
 var express =require('express');
 var router = express.Router();
+var productosModel=require('../models/pedidos.schema');
+var mongoose=require('mongoose');
 
-router.use(express.static('A&L motoristas'))
+router.use(express.static('A&L motoristas'));
+
+
+router.get('/pedidos/',function(req,res){
+    productosModel.find({}).then((data)=>{
+        res.send(data);
+        res.end();
+    })
+    .catch((error)=>{
+        res.send(error);
+        res.end();
+    })
+});
+
 
 module.exports = router;
